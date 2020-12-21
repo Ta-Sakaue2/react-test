885a0094e4815d3039450c3dd514c3f161af862a
Ta-Sakaue <takashi.sakaue@zenkigen.jp>
2020-12-21 20:08:10 +0900

    first add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import Header from './header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+  	  <button>Qiita情報取得</button>
+      <table>
+        <tr>
+          <th>タイトル</th>
+          <th>更新日</th>
+        </tr>
+        <tr>
+          <td>WordPressのログインページを変更する</td>
+          <td>2020-12-14</td>
+        </tr>
+        <tr>
+          <td>シャローコピーとディープコピー違い</td>
+          <td>2020-12-14</td>
+        </tr>
+      </table>
     </div>
   );
 }
---
+++ src/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <a href="https://qiita.com/">Qiitaへ</a>
+      </header>
+    );
+  }
+}
+
+export default Header;
